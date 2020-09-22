c0846a442ab783ea61eac2b450d27f7562ef8130
Nikita <47755204+niksonax@users.noreply.github.com>
2020-09-22 12:17:56 +0300

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,22 @@
+#include "calculator.h"
+
+int Calculator::Add (int a, int b)
+{
+    return a + b;
+}
+
+int Calculator::Mul (double a, double b)
+{
+    return a * b + 0.5;
+}
+
+//Such a lonely day
+//And it's mine
+//The most loneliest day of my life
+
+//Such a lonely day
+//Should be banned
+//It's day that I can't stand
+
+//The most loneliest day of my life
+//The most loneliest day of my life
